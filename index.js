1fb7111f8014f42fa4f548b8f2ea193a14e9c619
furbox <furbox@gmail.com>
2021-09-28 12:42:05 -0500

    v1

---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const pdfparse = require('pdf-parse');
+const info = require('./data.json');
+
+const pdffile = fs.readFileSync('2021-09-28_1.pdf');
+
+//get information
+const search = {};
+pdfparse(pdffile).then(function (data) {
+    // console.log(data.numpages)
+    // console.log(data.info)
+    // console.log(data.text)
+    const text = data.text;
+    let matching = info.filter(function (doc) {
+        const index1 = text.indexOf(doc.name);
+        const index2 = text.indexOf(doc.expediente);
+        if (index1 > 1 || index2 > 1) {
+            doc.ok = true;
+        } else {
+            doc.ok = false;
+        }
+    });
+    console.log(info);
+})
